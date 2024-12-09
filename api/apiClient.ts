8d6ee35605321d8ea67470c2cdde75f2a033abab
Mauricio Oliveira <ciumauri@gmail.com>
2024-12-09 08:16:04 -0300

    Configuração incicial apiClient

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage"; // Para armazenar o token no React Native
 
 const apiClient = axios.create({
     baseURL: "http://172.19.0.1:8000/api/", // URL base da sua API
@@ -7,23 +6,5 @@ const apiClient = axios.create({
         "Content-Type": "application/json",
     },
 });
-
-// Interceptor para adicionar o token JWT no cabeçalho Authorization
-apiClient.interceptors.request.use(
-    async (config) => {
-        try {
-            const token = await AsyncStorage.getItem("token"); // Obtenha o token do AsyncStorage
-            if (token) {
-                config.headers.Authorization = `Bearer ${token}`;
-            }
-        } catch (error) {
-            console.error("Erro ao buscar o token:", error);
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
 
 export default apiClient;
